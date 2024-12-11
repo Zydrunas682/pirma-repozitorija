f4e51e739335ce9d7679648a9b17fac5ed37a018
Zydrunas <Zydrunas@gmail.com>
2024-12-11 19:08:42 +0100

    11-12-24

---
+++ 21-11-24/namu darbai.py
@@ -0,0 +1,12 @@
+
+
+print("Labas,pasauli!")
+
+print("Sveikas,pasauli")
+
+print("gyveno karta mociute")
+print("Prie Eifelio boksto turejo ji buta")
+print("ji visko pasaulyje klausesi")
+print("bet girdejo tik kairia ausimi")
+
+# print("Naktis buvo salta! \nTodel jis kureno peciu")
